003a93fb7e5875e6ab6e48d1df87c8ab1d59f27b
pingping260687005 <pingping_01@hotmail.com>
2018-08-26 14:50:26 +0800

    create build

---
+++ build/app.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var $mysql = require("mysql");
+var sql = require("./sql"); //   这句话是，引入当前目录的mysql模板   mysql就是我们上面创建的mysql.js
+var $sql = $mysql.createConnection(sql.mysql); //创建一个连接        mysql是我们上面文件暴露出来的模板的方法
+$sql.connect(); //运用了这句才是真正连接
+//查询
+var select = "select * from mono"; //假设我们数据表叫mono  *代表查询全部内容  select查询
+$sql.query(select, function (err, res) {
+    if (err) {
+        console.log("错误", err); //我们打印出，错误信息  
+    }
+    else {
+        console.log(res); //打印出我们查询的内容
+    }
+});
+//增加
+var insert = "insert into mono (id,name,age) value (3,'中国',5000)"; //我们往数据表mono里面添加了一条数据；   id=3;name=中国;age=5000这是新的一条数据
+$sql.query(insert, function (err, res) {
+    if (err) {
+        console.log("错误", err);
+    }
+    else {
+        console.log("添加成功", res); //我们打印出添加成功的信息
+    }
+});
+//修改
+var updata = "update mono set name='中国' where name='北京' "; //我们把mono表里面name数据等于中国，改为了北京
+$sql.query(updata, function (err, res) {
+    if (err) {
+        console.log("错误", err);
+    }
+    else {
+        console.log("修改成功", res);
+    }
+});
+//删除
+var del = "delete from mono id=3"; //我们删除mono表里面id=3的数据  delete删除
+$sql.query(del, function (err, res) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("成功删除", res);
+    }
+});
+//# sourceMappingURL=app.js.map
---
+++ server/auction.server.ts
@@ -0,0 +1,43 @@
+import * as express from 'express';
+
+const app = express();
+
+// get
+app.get('/',(req,res)=>{
+
+    res.send('hello express');
+
+});
+
+app.get('/api/products',(req,res)=>{
+
+   // res.send('查询商品请求');
+    res.send(products);
+
+});
+
+app.get('/api/products/:id',(req,res)=>{
+   let p = products.find(product=>product.id == req.params.id)
+   console.log(p)
+   res.send(p);
+ });
+
+const server = app.listen(8000, "localhost",()=>{
+ 
+        console.log("服务器已经启动， 地址是localhost: 8000")
+});
+
+
+export class Product{
+    constructor(public id:number,public name:string,public  price: number, public rate:number,public desc:string) {}
+}
+ 
+
+ 
+    const products= new Array<Product>();
+    products.push(new Product(1, "第一件商品", 10.899, 3,  "第一件商品第一件商品第一件商品第一件商品第一件商品第一件商品"));
+    products.push(new Product(2, "第二件商品", 5.425, 4,  "第二件商品第二件商品第二件商品第二件商品第二件商品第二件商品"));
+    products.push(new Product(3, "第三件商品", 7.66, 4.5,  "第三件商品第三件商品第三件商品第三件商品第三件商品第三件商品"));
+    products.push(new Product(4, "第四件商品", 5.5, 3.5,  "第四件商品第四件商品第四件商品第四件商品第四件商品第四件商品"));
+    products.push(new Product(5, "第五件商品", 8.7, 5,  "第五件商品第五件商品第五件商品第五件商品第五件商品第五件商品"));   
+ 
