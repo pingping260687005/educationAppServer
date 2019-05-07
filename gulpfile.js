0714aedfadaf267ecd01396eb807ae95ac39dce1
pingping260687005 <pingping_01@hotmail.com>
2019-05-07 19:04:43 +0800

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp'),
+  nodemon = require('gulp-nodemon');
+// var gulp = require('gulp'),
+//   _ = require('lodash'),
+//   { updateRegistry, paths } = require('./gulp/common.js'),
+//   browserSync = require('browser-sync'),
+//   middleware = require('./gulp/proxy'),
+//   defaultAssets = require('./config/assets/default'),
+//   gulpLoadPlugins = require('gulp-load-plugins'),
+//   plugins = gulpLoadPlugins({
+//     rename: {
+//       'gulp-angular-templatecache': 'templateCache'
+//     }
+//   });
+// Nodemon task
+gulp.task('nodemon', () => {
+  return nodemon({
+    script: 'build/server.js',
+    nodeArgs: ['--inspect'],
+    args: ['--max-old-space-size=8192'],
+    ext: 'js,html',
+    verbose: true,
+    watch: ['build/**/*.js'],
+  });
+});
